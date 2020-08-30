a318ac5000c87860fed72a65e97b47bb4d3dff37
Niyantha <niyantha23052001@gmail.com>
2020-08-30 18:23:28 +0530

    Designed the countdown screen

--- app/src/main/java/com/ieeevit/enigma7/view/timer/CountdownActivity.kt
+++ app/src/main/java/com/ieeevit/enigma7/view/timer/CountdownActivity.kt
@@ -1,25 +1,23 @@
-package com.ieeevit.enigma7.view.auth
-
+package com.ieeevit.enigma7.view.timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.ieeevit.enigma7.R
-import com.ieeevit.enigma7.view.timer.CountdownFragment
+import com.ieeevit.enigma7.view.auth.ProfileSetupFragment
 
-class AuthActivity : AppCompatActivity() {
+class CountdownActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        setContentView(R.layout.activity_count_down)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         if (savedInstanceState == null) {
-            val fragment =CountdownFragment()
+            val fragment = CountdownFragment()
             supportFragmentManager.beginTransaction()
                 .add(R.id.container, fragment)
                 .commit()
         }
     }
-
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
