7388322153cc2444bdd347587d4607dbb41053bc
Niyantha <niyantha23052001@gmail.com>
2020-08-30 16:08:57 +0530

    Designed the profileSetup screen

--- app/src/main/java/com/ieeevit/enigma7/view/auth/AuthActivity.kt
+++ app/src/main/java/com/ieeevit/enigma7/view/auth/AuthActivity.kt
@@ -12,7 +12,7 @@ class AuthActivity : AppCompatActivity() {
         setContentView(R.layout.activity_auth)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         if (savedInstanceState == null) {
-            val fragment =SignUpFragment()
+            val fragment =ProfileSetupFragment()
             supportFragmentManager.beginTransaction()
                 .add(R.id.container, fragment)
                 .commit()
