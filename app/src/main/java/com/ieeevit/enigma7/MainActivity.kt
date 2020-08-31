64dce5600819bebc68fb4587ed8123c062c578ff
Ishan <axil.ishan3@gmail.com>
2020-09-01 00:05:31 +0530

    UI almost complete

--- app/src/main/java/com/ieeevit/enigma7/MainActivity.kt
+++ app/src/main/java/com/ieeevit/enigma7/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.ieeevit.enigma7
 
-import android.R
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 
