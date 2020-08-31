dfcf89b06dc55c495b998c4aa3f152321d16aed1
Niyantha <niyantha23052001@gmail.com>
2020-08-31 15:14:49 +0530

    profile screen

--- app/src/main/java/com/ieeevit/enigma7/view/auth/AuthActivity.kt
+++ app/src/main/java/com/ieeevit/enigma7/view/auth/AuthActivity.kt
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.ieeevit.enigma7.R
+import com.ieeevit.enigma7.view.home.ProfileFragment
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
         if (savedInstanceState == null) {
-            val fragment =SignUpFragment()
+            val fragment =ProfileFragment()
             supportFragmentManager.beginTransaction()
                 .add(R.id.container, fragment)
                 .commit()
