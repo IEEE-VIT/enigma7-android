a13986b07b839b567872947d1eb0d37c7f946a95
Ishan <axil.ishan3@gmail.com>
2020-09-07 16:03:58 +0530

    Minor UI changes #2

--- app/src/main/java/com/ieeevit/enigma7/view/splash/SplashActivity.kt
+++ app/src/main/java/com/ieeevit/enigma7/view/splash/SplashActivity.kt
@@ -12,7 +12,7 @@ class SplashActivity : AppCompatActivity() {
     private val SPLASH_TIME_OUT:Long = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_screen)
+        setContentView(R.layout.activity_splash)
         Handler().postDelayed({ startActivity(Intent(this, AuthActivity::class.java))
             finish()
         },SPLASH_TIME_OUT)
