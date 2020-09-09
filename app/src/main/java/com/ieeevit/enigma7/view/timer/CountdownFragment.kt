9ce45a9a8f9bf653dadd4c84fcd94682b18438b4
Niyantha <niyantha23052001@gmail.com>
2020-09-10 00:42:23 +0530

    implemented design changes in countdown

--- app/src/main/java/com/ieeevit/enigma7/view/timer/CountdownFragment.kt
+++ app/src/main/java/com/ieeevit/enigma7/view/timer/CountdownFragment.kt
@@ -2,6 +2,7 @@ package com.ieeevit.enigma7.view.timer
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -20,7 +21,8 @@ class CountdownFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        startActivity(Intent(activity, MainActivity::class.java))
+        // TODO: 10-09-2020 this is temporary navigation
+        Handler().postDelayed({startActivity(Intent(activity, MainActivity::class.java))},1000)
         return inflater.inflate(R.layout.fragment_countdown, container, false)
     }
 
