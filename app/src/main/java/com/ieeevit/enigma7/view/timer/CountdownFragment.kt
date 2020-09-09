fcc8f0ae172566a81035e976f0b2308ac36453c3
Niyantha <niyantha23052001@gmail.com>
2020-09-09 23:51:27 +0530

    navigation setup complete

--- app/src/main/java/com/ieeevit/enigma7/view/timer/CountdownFragment.kt
+++ app/src/main/java/com/ieeevit/enigma7/view/timer/CountdownFragment.kt
@@ -1,11 +1,13 @@
 package com.ieeevit.enigma7.view.timer
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ieeevit.enigma7.R
+import com.ieeevit.enigma7.view.main.MainActivity
 
 class CountdownFragment : Fragment() {
 
@@ -18,7 +20,7 @@ class CountdownFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-         // TODO: 31-08-2020 navigate to instruction
+        startActivity(Intent(activity, MainActivity::class.java))
         return inflater.inflate(R.layout.fragment_countdown, container, false)
     }
 
