a1534cfd954c96d82ccbfc161b6e37ab6a81de2d
Niyantha <niyantha23052001@gmail.com>
2020-11-19 18:31:46 +0530

    Fixed question response Model

--- app/src/main/java/com/ieeevit/enigma7/model/QuestionResponse.kt
+++ app/src/main/java/com/ieeevit/enigma7/model/QuestionResponse.kt
@@ -3,7 +3,7 @@ package com.ieeevit.enigma7.model
 import com.google.gson.annotations.SerializedName
 
 data class QuestionResponse(
-    @SerializedName("id") val id: Int,
+    @SerializedName("order") val id: Int,
     @SerializedName("img_url") val img_url: String,
     @SerializedName("text") val text: String
 )
